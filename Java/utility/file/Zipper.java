a099bb4af1722ac98e970b00c4f709990df66187
Abhineet1144 <mariobrain61@gmail.com>
2026-02-05 03:36:30 +0530

    Support for Zip folders

--- Java/utility/file/Zipper.java
+++ Java/utility/file/Zipper.java
@@ -10,6 +10,7 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zipper {
+
     private final FileOperationsMeta meta;
 
     public Zipper(FileOperationsMeta meta) {
@@ -30,40 +31,85 @@ public class Zipper {
     }
 
     public void zipFiles() throws IOException {
+
         File[] files = getFiles();
         File zipOutput = getZipOutput();
 
         long totalBytes = 0;
         for (File f : files) {
-            if (f != null && f.exists() && f.isFile()) {
-                totalBytes += f.length();
-            }
+            totalBytes += calculateSize(f);
         }
 
         long processedBytes = 0;
-        try (FileOutputStream fos = new FileOutputStream(zipOutput);
-             ZipOutputStream zos = new ZipOutputStream(fos)) {
+
+        try (FileOutputStream fos = new FileOutputStream(zipOutput); ZipOutputStream zos = new ZipOutputStream(fos)) {
 
             zos.setLevel(meta.getTotalChunks());
-            byte[] buffer = new byte[8192];
-            for (File file : files) {
-                if (file == null || !file.exists() || !file.isFile()) {
-                    continue;
-                }
-                try (FileInputStream fis = new FileInputStream(file)) {
-                    zos.putNextEntry(new ZipEntry(file.getName()));
-                    int len;
-                    while ((len = fis.read(buffer)) != -1) {
-                        zos.write(buffer, 0, len);
 
-                        processedBytes += len;
-                        meta.setPercentFinished((int) ((processedBytes * 100) / totalBytes));
-                    }
-                    zos.closeEntry();
-                }
+            byte[] buffer = new byte[8192];
+
+            for (File file : files) {
+                if (!file.exists()) continue;
+
+                processedBytes = zipRecursive(file, file.getParentFile(), zos, buffer, processedBytes, totalBytes);
             }
         }
+
         meta.setPercentFinished(100);
     }
-}
 
+    private long zipRecursive(File file, File baseDir, ZipOutputStream zos, byte[] buffer, long processedBytes, long totalBytes) throws IOException {
+
+        String entryName = baseDir == null ? file.getName() : baseDir.toURI().relativize(file.toURI()).getPath();
+
+        if (file.isDirectory()) {
+
+            if (!entryName.endsWith("/")) {
+                entryName += "/";
+            }
+
+            zos.putNextEntry(new ZipEntry(entryName));
+            zos.closeEntry();
+
+            File[] children = file.listFiles();
+            if (children != null) {
+                for (File child : children) {
+                    processedBytes = zipRecursive(child, baseDir, zos, buffer, processedBytes, totalBytes);
+                }
+            }
+
+        } else {
+
+            zos.putNextEntry(new ZipEntry(entryName));
+
+            try (FileInputStream fis = new FileInputStream(file)) {
+                int len;
+                while ((len = fis.read(buffer)) != -1) {
+                    zos.write(buffer, 0, len);
+
+                    processedBytes += len;
+                    meta.setPercentFinished((int) ((processedBytes * 100) / totalBytes));
+                }
+            }
+
+            zos.closeEntry();
+        }
+
+        return processedBytes;
+    }
+
+    private long calculateSize(File file) {
+        if (file == null || !file.exists()) return 0;
+
+        if (file.isFile()) return file.length();
+
+        long size = 0;
+        File[] children = file.listFiles();
+        if (children != null) {
+            for (File f : children) {
+                size += calculateSize(f);
+            }
+        }
+        return size;
+    }
+}
