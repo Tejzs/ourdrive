b02fc2ad2cfec59e78dccfeafa387bf911b4533f
Abhineet1144 <166810940+Abhineet1144@users.noreply.github.com>
2026-02-05 23:16:44 +0530

    Block access to files everyone
    * Add filter to block download request if you are not the owner
    
    * Bug fixes
    
    * Bug fixes

---
+++ Java/filters/FileAccessFilter.java
@@ -0,0 +1,27 @@
+package filters;
+
+import utility.SessionData;
+import utility.Utils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FileAccessFilter extends HttpFilter {
+    private static final int PRODNAME_PREFIX_LEN = "/FileStorage/Files/".length();
+
+    @Override
+    public void doFilter(HttpServletRequest servletRequest, HttpServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String requestURI = Utils.getRequestURI(servletRequest).substring(PRODNAME_PREFIX_LEN);
+        String mail = SessionData.getThreadLocalSessionData().getMail();
+
+        if (!requestURI.substring(0, requestURI.indexOf("/")).equals(mail)) {
+            servletResponse.sendRedirect("/FileStorage/Pages/unauthorized.html");
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
