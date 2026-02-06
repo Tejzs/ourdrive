c79f47b057ace91ccc8c01524d108ed324e9838c
Abhineet <abhineet.hariharan@zohocorp.com>
2026-02-07 04:05:32 +0530

    Bug fixes

--- Java/servlets/FileViewServlet.java
+++ Java/servlets/FileViewServlet.java
@@ -78,7 +78,7 @@ public class FileViewServlet extends HttpServlet {
         case "listSharedFiles":
             String sharedMainDir = request.getParameter("sharedMainDir");
             String subDirs = request.getParameter("sharedSubDirs");
-            requestedFile = new File(serverRootDir + "Files/" + sharedMainDir + subDirs);
+            requestedFile = new File(serverRootDir + sharedMainDir + subDirs);
             if (requestedFile.exists()) {
                 try {
                     AccessibilityHandler.getInstance().checkAccessible(sharedMainDir, mail);
