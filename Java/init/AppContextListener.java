7c49f3c55c0f15fac5032c67e0dc56662f9c5516
Tejzs <danduhman7@gmail.com>
2026-02-06 15:20:21 -0400

    modified config

--- Java/init/AppContextListener.java
+++ Java/init/AppContextListener.java
@@ -3,8 +3,6 @@ package init;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.slf4j.LoggerFactory;
-
 import config.Properties;
 import operations.FileOperationsProcessor;
 import utility.Utils;
@@ -13,7 +11,6 @@ public class AppContextListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        LoggerFactory.getLogger(AppContextListener.class).info("App Started");
         new Thread(new FileOperationsProcessor()).start();
         Properties.loadConfigurations(Utils.getServerHomeInServer(sce.getServletContext()));
     }
